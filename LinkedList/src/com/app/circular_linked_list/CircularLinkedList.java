4f9103051fc882cb8fec538ea886c5022f07650b
AkankshaSood27 <akanksha.sood7744@gmail.com>
2024-06-23 17:49:27 +0530

    Circular Linked List Implementation

--- LinkedList/src/com/app/circular_linked_list/CircularLinkedList.java
+++ LinkedList/src/com/app/circular_linked_list/CircularLinkedList.java
@@ -20,26 +20,25 @@ public class CircularLinkedList {
 	public CircularLinkedList() {
 		this.head = null;
 	}
-	
-	
+
 	public int display() throws LinkedListException {
-		Node temp=head;
+		Node temp = head;
 		int count = 0;
-		
-		if(head==null) {
+
+		if (head == null) {
 			throw new LinkedListException("List is empty!");
 		}
 		do {
 			count++;
-			System.out.print(temp.data+"-> ");
-			temp=temp.next;
-		}while(temp!=head);
+			System.out.print(temp.data + "-> ");
+			temp = temp.next;
+		} while (temp != head);
 		System.out.println(head.data);
 		return count;
 	}
 
 	// method to insert the node at the end of the circular linked list
-	public void insertAtEnd(int data) {
+	public void insert(int data) {
 		Node newNode = new Node(data);
 
 		// if the list is empty
@@ -88,7 +87,7 @@ public class CircularLinkedList {
 		}
 
 		Node prev = head;
-		for (int i = 1; i < pos-1; i++) {
+		for (int i = 1; i < pos - 1; i++) {
 			prev = prev.next;
 			if (prev == null) {
 				return false;
@@ -99,5 +98,129 @@ public class CircularLinkedList {
 		prev.next = newNode;
 		return true;
 
+	}
+
+	// search an element
+	public boolean search(int data) throws LinkedListException {
+		if (head == null) {
+			throw new LinkedListException("List is Empty!");
+		}
+		Node temp = head;
+		int count = 0;
+
+		do {
+			count++;
+			if (temp.data == data) {
+				System.out.println("The element is found at the position " + count);
+				return true;
+			}
+			temp = temp.next;
+		} while (temp != head);
+		return false;
+	}
+
+	// Delete by value
+	public boolean deleteByVal(int val) throws LinkedListException {
+		if (head == null) {
+			throw new LinkedListException("The list is empty!");
+		}
+		// if data to be deleted is present at the first node!
+		if (head.data == val) {
+			head = head.next;
+			return true;
+		}
+		Node prev = head;
+		Node del = head;
+		while (del.data != val) {
+			prev = del;
+			del = del.next;
+			if (del == null) {
+				throw new LinkedListException("Element not found!");
+			}
+		}
+
+		prev.next = del.next;
+		return true;
+	}
+
+	public boolean deleteByPosition(int position) throws LinkedListException {
+		// in case of empty linked list or invalid position
+		if (head == null) {
+			throw new LinkedListException("The List is already empty! Underflow");
+		}
+		if (position <= 0) {
+			throw new LinkedListException("The position entered is invalid! Please entered a valid position");
+		}
+
+		// in case of first position
+		if (position == 1) {
+			head = head.next;
+			return true;
+		}
+
+		Node prev = head;
+		for (int i = 1; i < position - 1; i++) {
+			prev = prev.next;
+			if (prev.next == head) {
+				throw new LinkedListException("Element not found!");
+			}
+		}
+		Node del = prev.next;
+		prev.next = del.next;
+		return true;
+
+	}
+
+	public int deleteLast() throws LinkedListException {
+		
+		int data;
+		// if list is empty
+		if (head == null) {
+			throw new LinkedListException("The List is already empty! Underflow");
+		}
+		Node temp = head;
+		Node prev = null;
+		
+		//if there is only one node in the list
+		if(head.next==head) {
+			data=head.data;
+			head=null;//list will be empty
+			return data;
+		}
+		//traverse till you reach the last node
+		do {
+			prev=temp;
+			temp=temp.next;
+			
+		}while(temp.next!=head);
+		
+		//remove the last node
+		data=temp.data;
+		prev.next=head;
+		return data;
+	}
+	
+	
+	
+	public void reverse() {
+		
+		//there is only one node in the list
+		if(head==null||head.next==null) {
+			return;
+		}
+		Node prev=null;
+		Node current=head;
+		Node next=null;
+		//Node tail=head;
+		
+		do {
+			next=current.next;
+			current.next=prev;
+			prev=current;
+			current=next;
+		}while(current!=head);
+		
+		head.next=prev;
+		head=prev;
 	}
 }
--- LinkedList/src/com/app/circular_linked_list/Test.java
+++ LinkedList/src/com/app/circular_linked_list/Test.java
@@ -2,8 +2,6 @@ package com.app.circular_linked_list;
 
 import java.util.Scanner;
 
-import com.app.doubly_linked_list.DoublyLinkedList;
-
 public class Test {
 
 	public static void main(String[] args) {
@@ -12,52 +10,63 @@ public class Test {
 			boolean exit = false;
 			CircularLinkedList l = new CircularLinkedList();
 
-			// adding values to the list at the end
-			l.insertAtEnd(10);
-			l.insertAtEnd(20);
-			l.insertAtEnd(30);
-			l.insertAtEnd(40);
-			l.insertAtEnd(50);
+			// adding values to the list at the ends
+			l.insert(10);
+			l.insert(20);
+			l.insert(30);
+			l.insert(40);
+			l.insert(50);
 
 			while (!exit) {
 
 				System.out.println("\nEnter your choice: ");
 				System.out.println("1.Display\n" + "2.Insertion at the end of the list\n"
-						+ "3.Insertion at the beginning\n" + "4.Insertion at a particular position\n"
-						+ "5.Search an element\n" + "6.Deletion by value\n"
-						+ "7.Deletion by position\n" + "8.Delete the first node\n"
-						+ "9.Delete the last node\n" + "10.Reversing the given linked list- iterative\n"
-						+ "11.Reverse the list by recursion\n" + "12.Exit\n");
-				
+						+ "3.Insertion at a particular position\n" + "4.Search an element\n" + "5.Deletion by value\n"
+						+ "6.Deletion by position\n" + "7.Delete the last node\n"
+						+ "8.Reversing the given linked list- iterative\n" + "9.Exit\n");
+
 				try {
 					switch (sc.nextInt()) {
 					case 1:
-						int count=l.display();
-						System.out.println("No of nodes in the list is: "+count);
+						int count = l.display();
+						System.out.println("No of nodes in the list is: " + count);
 						break;
 					case 2:
+						System.out.println("Enter the element you want to add at the end: ");
+						l.insert(sc.nextInt());
 						break;
 					case 3:
-						break;
-					case 4:
 						System.out.println("Enter the element and its position: ");
 						l.insertAtPosition(sc.nextInt(), sc.nextInt());
 						break;
+					case 4:
+						System.out.println("Enter the element you want to search: ");
+						boolean result = l.search(sc.nextInt());
+						if (!result) {
+							System.out.println("The element is not found!");
+						}
+						break;
 					case 5:
+						System.out.println("Enter the element you want to delete: ");
+						if (l.deleteByVal(sc.nextInt())) {
+							System.out.println("Element deleted successfully!");
+						}
 						break;
 					case 6:
+						System.out.println("Enter the position at which you want to delete the element: ");
+						if (l.deleteByPosition(sc.nextInt())) {
+							System.out.println("Element deleted successfully!");
+						}
 						break;
 					case 7:
+						int del = l.deleteLast();
+						System.out.println(del + " has been removed from the list!");
 						break;
 					case 8:
+						l.reverse();
+						l.display();
 						break;
 					case 9:
-						break;
-					case 10:
-						break;
-					case 11:
-						break;
-					case 12:
 						exit = true;
 						System.out.println("Exitting...\nSuccessfull!");
 						break;
